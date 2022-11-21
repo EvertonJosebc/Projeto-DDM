9541f107d8f0213dfb6289f2b96634bfbf48b0e8
ÉVERTON JOSÉ BENTO CORREIA <everton.jose@academico.ifrn.edu.br>
2022-11-21 20:34:35 -0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, Touchable, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
+  function diminuir(){
+    if(count>0){
+      setCount(count-1)
+    }
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.texto}>Quantidade de pessoas: { count } </Text>
+      <TouchableOpacity style={styles.button} onPress = { ()=> setCount(count + 1)}>
+        <Text style={styles.botoes}>+</Text>
+      </TouchableOpacity>
+      
+        <TouchableOpacity style={styles.button} onPress = { ()=> diminuir( )}>
+          <Text style={styles.botoes}>-</Text>
+        </TouchableOpacity>
+      
     </View>
   );
 }
@@ -14,8 +29,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    width: '300px',
+    margin: 'auto',
+    backgroundColor: '#483D8B',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  texto: {
+    color: 'white',
+    backgroundColor: '#191970',
+    padding: '10px',
+    width: '100%',
+    textAlign: 'center',
+    fontSize: '20px',
+    fontFamily: 'roboto',
+  },
+  botoes: {
+    color: 'white',
+    padding: '15px',
+    backgroundColor: '#6A5ACD',
+    width: '200px',
+    textAlign: 'center',
+    marginTop: '15px',
+    borderRadius: '5px',
+    fontSize: '18px',
   },
 });
